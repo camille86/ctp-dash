e0f0016408b10e7105682be7541d42418e264a52
camille86 <camille.seaberry@gmail.com>
2017-04-14 16:30:03 -0400

    Fix links in index.js

--- js/index.js
+++ js/index.js
@@ -78,7 +78,7 @@ function render(d, i) {
     var card = d;
     card.current_val = makeFormat(d.format, +d.current_val);
     card.prev_val = d.prev_val.length ? makeFormat(d.format, +d.prev_val) : '';
-    card.area = '<a class="area" href="/pages/' + d.class + '.html">' + d.area + '</a>';
+    card.area = '<a class="area" href="pages/' + d.class + '.html">' + d.area + '</a>';
     card.arrow = '<span class="glyphicon glyphicon-arrow-' + d.arrow + '"></span>';
     console.log(card);
 
